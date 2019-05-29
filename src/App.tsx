5975c0a2cccfdc60a80fc4e8943a9a6e3c4e7204
Valentin Lungenstrass <valentin.lungenstrass@abbeal.com>
2019-05-29 15:31:55 +0200

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+interface Props {}
+export default class App extends Component<Props> {
+
+  render() {
+    return (
+      <Text>Hello !</Text>
+    );
+  }
+}
